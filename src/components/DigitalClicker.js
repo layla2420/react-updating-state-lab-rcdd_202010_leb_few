6d78ebbef0d0e6028c5c410a1e9c929baad423d1
layla  hammoud <laylahammoud63@gmail.com>
2021-03-16 15:21:32 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+ class DigitalClicker extends React.Component {
+  constructor() {
+    this.state={
+      timeClicked:0,
+    }
+
+function handleincrement(){
+this.setState(prev=>({timeClicked:prev.timeClicked+1}))
+}
+    
+  }
+  render(){(
+  <button onClick={this.handleincrement}>{this.state.timeClicked}</button>
+  )
+}}
+export default DigitalClicker;
